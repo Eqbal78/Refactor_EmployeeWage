d15fe1e61528fb2ae78d32c46aa315ca83ecdde2
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-06 19:44:31 +0530

    Refactor Add PartTime Employee

--- RefactorEmployeeWage/RefactorEmployeeWage/Program.cs
+++ RefactorEmployeeWage/RefactorEmployeeWage/Program.cs
@@ -5,15 +5,20 @@ namespace RefactorEmployeeWage
     class Program
     {
         static int Full_Time = 1;
+        static int Part_Time = 2;
         static int Emp_rate_per_Hrs = 20;
         static int empHrs, empWage;
         static int Attendance()
         {
             Random r = new Random();
-            int check = r.Next(1, 3);
+            int check = r.Next(1, 4);
             if (check == Full_Time)
             {
                 empHrs = 8;
+            }
+            else if (check == Part_Time)
+            {
+                empHrs = 4;
             }
             else
                 empHrs = 0;
