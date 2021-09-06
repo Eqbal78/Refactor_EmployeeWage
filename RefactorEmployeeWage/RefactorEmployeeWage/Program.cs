a881ef9f7c30c702813a84231981c0003b09cb4c
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-06 19:58:05 +0530

    Refactor Calculate Employee Monthly Wages

--- RefactorEmployeeWage/RefactorEmployeeWage/Program.cs
+++ RefactorEmployeeWage/RefactorEmployeeWage/Program.cs
@@ -8,30 +8,37 @@ namespace RefactorEmployeeWage
         static int Part_Time = 2;
         static int Emp_rate_per_Hrs = 20;
         static int empHrs, empWage;
+        static int No_Of_Working_Days = 20;
+        static int totalEmpWage = 0;
         static int Attendance()
         {
-            Random r = new Random();
-            int check = r.Next(1, 4);
-            switch (check)
+            for (int i = 0; i < No_Of_Working_Days; i++)
             {
-                case 1:
-                    empHrs = 8;
-                    break;
-                case 2:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            empWage = empHrs * Emp_rate_per_Hrs;
+                Random r = new Random();
+                int check = r.Next(1, 4);
+                switch (check)
+                {
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
 
-            return empWage;
+                empWage = empHrs * Emp_rate_per_Hrs;
+                totalEmpWage += empWage;
+            }
+            return totalEmpWage;
         }
         static void Main(string[] args)
         {
             Attendance();
-            Console.WriteLine("Employee Wages: " + empWage);
+            //Console.WriteLine("Employee Wages: " + empWage);
+            Console.WriteLine("Total Monthly Emp Wages: " + totalEmpWage);
         }
     }
 }
