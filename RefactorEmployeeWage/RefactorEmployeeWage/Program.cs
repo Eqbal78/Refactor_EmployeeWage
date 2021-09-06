31f1d5de859b946ad5ecfc9b3b46fc28f8be71ec
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-06 19:26:15 +0530

    Refactor Employee Attendances

---
+++ RefactorEmployeeWage/RefactorEmployeeWage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RefactorEmployeeWage
+{
+    class Program
+    {
+        static int Is_Full_Time = 1;
+        static void Attendance()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            Console.WriteLine("Welcome to Employee Wage Computation Program using C#");
+            if (empcheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emplyoee is Absent");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Attendance();
+        }
+    }
+}
